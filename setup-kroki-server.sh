345bf78a70e72f363f94a1eba381812b2a50d3b4
Vysakh Pillai <vysakhpillai@embeddedinn.com>
2025-03-28 00:25:47 -0700

    update nginx config
    Signed-off-by: Vysakh Pillai <vysakhpillai@embeddedinn.com>

--- setup-kroki-server.sh
+++ setup-kroki-server.sh
@@ -58,7 +58,11 @@ server {
     }
 
     location /try {
-        proxy_pass http://demosite:8006;
+        proxy_pass http://demosite:8006/index.html;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto https;
     }
 }
 }
